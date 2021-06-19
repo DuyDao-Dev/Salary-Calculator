310a8a7a4797b6ae7ad0d96af1815fec9accaa62
Duy Dao <jimmydao@gmail.com>
2021-06-19 16:12:19 -0500

    Starting to get it. Got stuck on deleting row. Had to ask for help.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+
 $(document).ready(onReady);
 
-let employee = {
-    first: firstName, 
-    last: lastName,
-    number: employeeID,
-    title: employeeTitle,
-    salary: annualSalary
-}
+
+
+let employeeArray= [];
+console.log(employeeArray);
 
 function onReady(){
     console.log("Starting the weekend challenge!");
 
     //create form to input employee's firstName, lastName, idNumber, employeeTitle, annualSalary.
+    // finished this in HTML
 
     //create a SUBMIT button to send to the DOM. Should clear form when click on SUBMIT. 
     //Should also collect that information to calculate monthly cost of combined salaries 
@@ -21,13 +20,49 @@ function onReady(){
     //
 }
 
-function submitEmployee(employee){
-    console.log(employee)
-    $('.submit-employee').on('click', submit)
-    $('.submit-employee').empty();
+
+
+function submitEmployee(){
+//the employee object
+//document.getElementById('firstName').value// this would how it would look without jQuery
+let employee = {
+    firstName: $('#firstName').val(), 
+    lastName: $('#lastName').val(),
+    employeeID: $('#employeeID').val(), 
+    employeeTitle: $('#employeTitle').val(),
+    annualSalary: $('#annualSalry').val(), 
 }
 
-function monthlyCalc (){
-    $('#annualSalary').append('<p>class="box"</p> >')
+employeeArray.push(employee);
+
+//draw the table
+markup = "<tr id=employeeRow"+employee.employeeID+">" //opening row tag, identify a row by the employeeID
+markup += "<td>"+employee.firstName+"</td>" // one cell in that row //first Name
+markup += "<td>This is a row</td>" // one cell in that row //Last Name
+markup += "<td>This is a row</td>" // one cell in that row //first Name
+markup += "<td>This is a row</td>" // one cell in that row //first Name
+markup += "<td>This is a row</td>" // one cell in that row //first Name
+markup += "<td> <button id="+employee.employeeID+" onClick='deleteRow(event)'>Delete</button></td>" // one cell in that row //Delete a  button
+
+markup += "</tr>"; // closing row tag
+tableBody = $("#employeeTable");
+tableBody.append(markup);
+                
+console.log(employeeArray)
 }
 
+function deleteRow(e){
+	/* console.log(e.target.id) */
+	console.log('delete employee ID:' + e.target.id)
+$( "#employeeRow"+e.target.id).remove();
+  
+}
+
+function salaryCalc (){
+    $('#annualSalary').append('<p>class="box"</p>')
+    //need to print to DOM annual salary and something to calculate the monthly.
+}
+
+function clearBoxes (){
+$('.submit-employees').empty();
+}
