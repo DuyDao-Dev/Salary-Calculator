423711ba9cd48b4ed96a62038918ef4dcc574589
Duy Dao <jimmydao@gmail.com>
2021-06-19 17:52:01 -0500

    Updated my tables to match employee objects. Note to self that I suck at spelling after 10 hours of coding. Please take breaks

--- script.js
+++ script.js
@@ -29,25 +29,29 @@ let employee = {
     firstName: $('#firstName').val(), 
     lastName: $('#lastName').val(),
     employeeID: $('#employeeID').val(), 
-    employeeTitle: $('#employeTitle').val(),
-    annualSalary: $('#annualSalry').val(), 
+    employeeTitle: $('#employeeTitle').val(),
+    annualSalary: Number($('#annualSalary').val()), 
 }
 
 employeeArray.push(employee);
-
+salaryCalc();
 //draw the table
 markup = "<tr id=employeeRow"+employee.employeeID+">" //opening row tag, identify a row by the employeeID
 markup += "<td>"+employee.firstName+"</td>" // one cell in that row //first Name
-markup += "<td>This is a row</td>" // one cell in that row //Last Name
-markup += "<td>This is a row</td>" // one cell in that row //first Name
-markup += "<td>This is a row</td>" // one cell in that row //first Name
-markup += "<td>This is a row</td>" // one cell in that row //first Name
+markup += "<td>"+employee.lastName+"</td>" // one cell in that row //Last Name
+markup += "<td>"+employee.employeeID+"</td>" // one cell in that row //first Name
+markup += "<td>"+employee.employeeTitle+"</td>" // one cell in that row //first Name
+markup += "<td>"+employee.annualSalary+"</td>" // one cell in that row //first Name
 markup += "<td> <button id="+employee.employeeID+" onClick='deleteRow(event)'>Delete</button></td>" // one cell in that row //Delete a  button
 
 markup += "</tr>"; // closing row tag
 tableBody = $("#employeeTable");
 tableBody.append(markup);
-                
+$('#firstName').val('');//is there a way to consolidate this?
+$('#lastName').val('');
+$('#employeeID').val('');
+$('#employeeTitle').val('');
+$('#annualSalary').val('');
 console.log(employeeArray)
 }
 
@@ -55,14 +59,42 @@ function deleteRow(e){
 	/* console.log(e.target.id) */
 	console.log('delete employee ID:' + e.target.id)
 $( "#employeeRow"+e.target.id).remove();
-  
+
+	//still need to find and remove from employeeArray
+  //need to loop through employeeArray
+  //find the employeeID that matches e.target.id
+  //remove that specific index from the array
+  //to remove use the splice function
+  //employeeArray.splice(matchingIndex, 1);
+  for (let index = 0; index < employeeArray.length; index++) {
+    const employee = employeeArray[index];
+    	if (employee.employeeID==e.target.id){
+      		var matchingIndex = index;//why does var work and not let?
+          break;
+      }
+ }
+ console.log(matchingIndex);
+employeeArray.splice(matchingIndex, 1);
 }
 
 function salaryCalc (){
-    $('#annualSalary').append('<p>class="box"</p>')
-    //need to print to DOM annual salary and something to calculate the monthly.
+    let totalSalary = 0;
+    //I need to add total of all employees' annualSalary and then divide by 12
+    for (let index = 0; index < employeeArray.length; index++) {
+        const employeeSalary = employeeArray[index]['annualSalary'];
+        totalSalary += employeeSalary;//need total first
+    }
+    totalSalary = totalSalary / 12;//then divide by 12
+    $('#monthlyCost').html('Total Monthly: ' + 
+    (totalSalary).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',}));//Got help with this since I couldn't figure out
+    //how to do commas and decimal limit. Pretty cool that they have one
+    //depending on the country you're in. 
+     
 }
 
-function clearBoxes (){
-$('.submit-employees').empty();
-}
+
+console.log(employeeArray);
+
+//note to self. You spell poorly when you're on this for 10 hours. 
